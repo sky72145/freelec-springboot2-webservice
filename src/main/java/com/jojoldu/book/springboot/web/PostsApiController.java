ade46f5c0adea25a7e761d0ee7df9e8cf51200b3
sky72145 <sky7214sky72@naver.com>
2020-08-20 22:35:20 +0900

    20200726 3장 등록기능까지

---
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.springboot.web;
+
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor//final이나 nonnull인 필드값만 파라미터로 받는 생성자를 만들어주는 lombok 어노테이션
+@RestController//문자열, JSON등을 ResponseBdy 어노테이션 없이 전송가능
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,30 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor//파라미터가 없는 기본 생성자 만들어주는 lombok
+@Getter
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+
+}
